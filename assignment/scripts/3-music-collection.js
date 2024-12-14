93388bc7c65993f5f97e69bd27ecff282f72732b
Bryan McGee <mcgee.bryan.andrew@gmail.com>
2024-12-14 11:22:56 -0600

    Finished up the first part

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,32 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
 
+function addToCollection (collection, title, artist, yearPublished) {
+  //The above inputs need to be added to a new object
+    //-collection, that is where it gets pushed to
+  //Title, then artist, then yearPublished
+  //This gets pushed to the collection specified
+  const newItem = {
+    title,
+    artist,
+    yearPublished,
+  };
+  
+  collection.push(newItem)
+  return newItem
+};
 
-
-
-
+//Turns out that I could not use a function for testing the function...
+addToCollection (myCollection, "Forever", "Mystery Skulls", 2014)
+addToCollection (myCollection, "You're Awful, I Love You", "Ludo", 2008);
+addToCollection (myCollection, "Twenty One Pilots", "Twenty One Pilots", 2009);
+addToCollection (myCollection, "Black Holes and Revelations", "Muse", 2006);
+addToCollection (myCollection, "exuviae", "Darius", 2019);
+addToCollection (myCollection, "Slasher", "Savant", 2018);
+  
+console.log(myCollection);
 
 
 
